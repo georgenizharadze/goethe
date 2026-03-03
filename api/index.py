beb4f570f3171254d8762c091f5dfba6caea090b
George Nizaradze <georgenizharadze@sky.com>
2026-03-03 12:04:45 +0100

    Add LangSmith client

--- api/index.py
+++ api/index.py
@@ -1,8 +1,21 @@
+import os
 from fastapi import FastAPI  # type: ignore
 from fastapi.responses import PlainTextResponse  # type: ignore
 from pydantic import BaseModel  # type: ignore
 from openai import OpenAI  # type: ignore
 from langgraph_sdk.client import get_client
+
+LANGSMITH_API_KEY = os.getenv("LANGSMITH_API_KEY")
+LANGSMITH_API_URL = os.getenv("LANGSMITH_API_URL")
+LANGSMITH_AGENT_ID = os.getenv("LANGSMITH_AGENT_ID")
+
+langsmith_client = get_client(
+    url=LANGSMITH_API_URL,
+    api_key=LANGSMITH_API_KEY,
+    headers={
+        "X-Auth-Scheme": "langsmith-api-key",
+    },
+)
 
 app = FastAPI()
 
